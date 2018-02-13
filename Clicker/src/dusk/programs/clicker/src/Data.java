a161233dc54ed88ac0a72bb8ce584238c3f40d53
D-u-s-k <ramenrampage@tuta.io>
2018-02-13 21:45:31 +0000

    Added data class

---
+++ Clicker/src/dusk/programs/clicker/src/Data.java
@@ -0,0 +1,18 @@
+package dusk.programs.clicker.src;
+
+public class Data {
+	
+	//Generic data
+	public static int item;
+	public static int upgradeNumber;
+	public static int buildingNumber;
+	public static int achievementNumber;
+	
+	//Achievement Data
+	
+	//Upgrade data
+	
+	//Building Data
+	
+
+}
